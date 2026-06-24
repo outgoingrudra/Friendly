d8e3a82841774430b33c16d0f40deefed797078e
Rudra Verma <rudraverma781@gmail.com>
2026-06-24 20:12:41 +0530

    updated import error in backend

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -1,4 +1,4 @@
-import Chat from "../models/Chat.js";
+import Chat from "../models/chat.js";
 import Connection from "../models/connection.js";
 import User from "../models/user.js";
 export async function getChat(req, res) {
